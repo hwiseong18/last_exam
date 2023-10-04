51969492bfa65b83a4fa510f1a66350a66255246
hwiseong18 <leehs050725@naver.com>
2023-10-04 14:22:48 +0900

    231004

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -3,10 +3,18 @@ import { ObjectId } from "mongodb";
 
 export default async function Detail({params}) {
   const db = await client.db('exam');
-  const result = await db.collection('problem').find({'_id':new ObjectId(params.id)})
+  const result = await db.collection('problem').findOne({'_id':new ObjectId(params.id)})
   return (
-    <div className="detail">
-      {result.content}
-    </div>
+    <>
+      <h2 style={{padding:"15px", margin:"15px"}}>여기가 상세</h2>
+      <center><div className='detail'>
+        <div>{result.content}</div>
+        <div>{result.img}</div>
+        <div>1. {result.first}</div>
+        <div>2. {result.second}</div>
+        <div>3. {result.third}</div>
+        <div>4. {result.fourth}</div>
+      </div></center>
+    </>
   )
 }
